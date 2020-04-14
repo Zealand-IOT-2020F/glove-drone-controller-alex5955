fc78a05f6ee405dbebe0162316cc1376235ff4c3
alex5955 <43375012+alex5955@users.noreply.github.com>
2020-04-14 16:02:06 +0200

    Sensehat controller

--- Drone.py
+++ Drone.py
@@ -2,13 +2,77 @@ import socket
 import sys
 import time
 
-
 class Drone(object):
-    """description of class"""
 
+    def __init__(self, ip,port):
+        self.TelloIp = ip
+        print("ip: " + ip)
+        self.TelloPort = port
+        
+        # Create a UDP socket
+        self.Host =""
+        self.HostPort = 9000
+        self.locaddr = (self.Host,self.HostPort)
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        #self.tello_address = ('192.168.10.1', 8889)
+        self.tello_address = (ip, port)
+        self.sock.bind(self.locaddr)
+
+    def sendCommand(self,TelloMessage):
+        # print Command recived
+        print("send message "+ TelloMessage +" end")
+        msg = TelloMessage.encode(encoding="utf-8")
+
+        # Sends / Recives data from drone
+        sent = self.sock.sendto(msg,self.tello_address)
+        data, server = self.sock.recvfrom(1518)
+        
+        # Return data to caller
+        print(data.decode(encoding="utf-8"))
+        return "from sendmessage " + TelloMessage + " end "
+
+    def connect(self):
+        print("Connect")
+        result = self.sendCommand("command")
+        print (result)
 
     def takeOff(self):
-        print("takeOff")
-        #add the takeoff code
+        self.sendCommand("takeoff")
 
+    def land(self):
+        self.sendCommand("land")
+
+    def battery(self):
+        bat = self.sendCommand("battery?")
+        return bat
+
+    def end(self):
+        print("close")
+        self.sock.close()
+        
+    def forward(self, amount):
+        self.sendCommand("forward " + str(amount))
+
+    def back(self, amount):
+        self.sendCommand("back " + str(amount))
+
+    def rotateCW(self,amount):
+        self.sendCommand("cw" + str(amount))
+
+    def rotateCCW(self,amount):
+        self.sendCommand("ccw" + str(amount))
+
+    def up(self, amount):
+        result = self.sendCommand("up " + str(amount)) 
+
+    def down(self, amount):
+        result = self.sendCommand("down " + str(amount)) 
+
+    def printinfo(self):
+        print("Hallo Drone at : IP:" + str(self.TelloIp) + " Port: " + str(self.TelloPort))
+
+    def left(self, amount):
+        self.sendCommand("left " + str(amount))
     
+    def right(self, amount):
+        self.sendCommand("right " + str(amount))
--- Main.py
+++ Main.py
@@ -1,2 +1,54 @@
+import Drone
+import time
+from sense_hat import SenseHat
+sense = SenseHat()
 
-#here you should interact with the drone
+drone1 = Drone.Drone('192.168.10.1',8889)
+
+drone1.connect()
+
+#Info printed
+drone1.battery()
+drone1.printinfo()
+
+######CONTROLS:#######
+#3 gange på enter knappen til at takeoff og lande igen
+# up/down key for at komme op/ned 20 cm, left/right key for at rotere 45 deg ccw/cw
+
+countFlying = 0
+takeoff = False
+
+while True:
+    for event in sense.stick.get_events():
+        #print(event.direction, event.action)
+        if event.direction == "middle" and event.action == "released" and takeoff == False:
+          countFlying += 1
+          print("count flying: " + str(countFlying))
+          if countFlying == 3:
+            drone1.takeOff()
+            countFlying = 0
+            takeoff = True
+            break
+            
+        if event.direction == "middle" and event.action == "released" and takeoff == True:
+          countFlying += 1
+          print("count flying: " + str(countFlying))
+          if countFlying == 3:
+            drone1.land()
+            countFlying = 0
+            takeoff = False
+            
+        if event.direction == "up" and event.action == "released" and takeoff == True:
+          drone1.up(20)
+        
+        if event.direction == "down" and event.action == "released" and takeoff == True:
+          drone1.down(20)
+          
+        if event.direction == "left" and event.action == "released" and takeoff == True:
+          drone1.rotateCCW(45)
+          
+        if event.direction == "right" and event.action == "released" and takeoff == True:
+          drone1.rotateCW(45)
+        
+#end program
+drone1.end()
